9ec72b01dd52cea6cb13d5fe3eea7d4e12769b04
Alvaro López Medina <alopezme@redhat.com>
2022-01-31 18:05:52 +0100

    Add script to automate package and deploy

--- package-and-deploy.sh
+++ package-and-deploy.sh
@@ -2,37 +2,43 @@
 
 set -e
 
-CHART_NAME="$1"
-CHART_NAME="java-hello-world"
+JAVA_CHART_NAME="java-hello-world"
+PYTHON_CHART_NAME="python-hello"
 PACKAGE="${CHART_NAME}-0.1.0.tgz"
 MASTER_BRANCH="demo"
 PKGS_BRANCH="gh-pages"
 
-cd ${CHART_NAME}
+echo "Package and deploy: $JAVA_CHART_NAME and $PYTHON_CHART_NAME"
 
-echo "Package and deploy: ${CHART_NAME}"
-
-echo -e "\n[1/4] Checkout master, package and stash"
+echo -e "\n[1/6] Checkout master, package and stash"
 git checkout $MASTER_BRANCH
 git pull -a
+
+echo -e "\n[2/6] Package $JAVA_CHART_NAME"
+cd $JAVA_CHART_NAME
 helm dependencies update
 helm package .
-git add -f ${PACKAGE}
-git stash push ${PACKAGE}
+cp ${JAVA_CHART_NAME}-0.1.0.tgz /tmp
+cd -
 
-echo -e "\n[2/4] Checkout gh-pages and update package"
+echo -e "\n[3/6] Package j$PYTHON_CHART_NAME"
+cd $PYTHON_CHART_NAME
+helm dependencies update
+helm package .
+cp ${PYTHON_CHART_NAME}-0.1.0.tgz /tmp
+cd -
+
+echo -e "\n[4/6] Checkout gh-pages and update package"
 git checkout $PKGS_BRANCH
-rm -rf ${PACKAGE}
-git add *.tgz
-git stash pop
-git add *.tgz
-git clean -fd
+rm -rf helm-packages/*
+cp /tmp/${JAVA_CHART_NAME}-0.1.0.tgz helm-packages/
+cp /tmp/${PYTHON_CHART_NAME}-0.1.0.tgz helm-packages/
 
-echo -e "\n[3/4] Update Helm Index"
+echo -e "\n[5/6] Update Helm Index"
 helm repo index . --url https://shellclear.github.io/helm/
 git add index.yaml
 
-echo -e "\n[4/4] Git commit and push"
+echo -e "\n[6/6] Git commit and push"
 git commit -m "Update package to the latest commit on master"
 # git push
 
