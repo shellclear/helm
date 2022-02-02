e8783d961b995ba4cfa863c7a0dc7ebbb50cb892
shellclear <12013211+shellclear@users.noreply.github.com>
2022-02-02 13:34:24 +0100

    changes to package-and-deploy.sh file

--- package-and-deploy.sh
+++ package-and-deploy.sh
@@ -11,40 +11,29 @@ PKGS_BRANCH="gh-pages"
 echo "Package and deploy: $JAVA_CHART_NAME and $PYTHON_CHART_NAME"
 
 echo -e "\n[1/6] Checkout master, package and stash"
+git stash
 git checkout $MASTER_BRANCH
 git pull -a
 
 echo -e "\n[2/6] Package $JAVA_CHART_NAME"
 cd $JAVA_CHART_NAME
 helm dependencies update
-helm package .
-cp ${JAVA_CHART_NAME}-0.1.0.tgz /tmp
-rm -rf ${JAVA_CHART_NAME}-0.1.0.tgz
-cd -
+helm package $JAVA_CHART_NAME $PYTHON_CHART_NAME -d /tmp
 
-echo -e "\n[3/6] Package $PYTHON_CHART_NAME"
-cd $PYTHON_CHART_NAME
-helm dependencies update
-helm package .
-cp ${PYTHON_CHART_NAME}-0.1.0.tgz /tmp
-rm -rf ${PYTHON_CHART_NAME}-0.1.0.tgz
-cd -
-
-echo -e "\n[4/6] Checkout gh-pages and update package"
+echo -e "\n[3/6] Checkout gh-pages and update package"
 git checkout $PKGS_BRANCH
-rm -rf helm-packages/*
-cp /tmp/${JAVA_CHART_NAME}-0.1.0.tgz helm-packages/
-cp /tmp/${PYTHON_CHART_NAME}-0.1.0.tgz helm-packages/
+cp -f /tmp/${JAVA_CHART_NAME}-0.1.0.tgz helm-packages/
+cp -f /tmp/${PYTHON_CHART_NAME}-0.1.0.tgz helm-packages/
 git add -A helm-packages/
 
-echo -e "\n[5/6] Update Helm Index"
-helm repo index . --url https://shellclear.github.io/helm/
+echo -e "\n[4/6] Update Helm Index"
+helm repo index --merge --url https://shellclear.github.io/helm/ .
 git add index.yaml
 
-echo -e "\n[6/6] Git commit and push"
-git commit -m "Update package to the latest commit on master"
-git push
+# echo -e "\n[5/6] Git commit and push"
+# git commit -m "Update packages and index files to the latest commit on master"
+# git push
 
-echo -e "\n[END] Return to previous state"
-git checkout $MASTER_BRANCH
-cd -
+# echo -e "\n[END] Return to previous state"
+# git checkout $MASTER_BRANCH
+# cd -
